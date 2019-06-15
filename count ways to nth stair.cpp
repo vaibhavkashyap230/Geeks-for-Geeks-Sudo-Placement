20336c47f688b027ca94464429c969dac67fcbf2
Vaibhav Kashyap <vaibhavkashyap230@gmail.com>
2019-06-15 16:51:27 +0530

    Add files via upload

---
+++ count ways to nth stair.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	for(int x=0;x<t;x++)
+	{
+	    int n;
+	    cin>>n;
+	    n=n/2;
+	    cout<<n+1<<endl;
+	}
+	return 0;
+}
